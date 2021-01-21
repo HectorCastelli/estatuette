a0493388cae7ad64b87314108ebe0afe507af4b1
Hector Castelli Zacharis <hector.zacharia@gmail.com>
2021-01-21 20:30:21 +0100

    added wall breaking effect

--- Scenario/BreakableWall.gd
+++ Scenario/BreakableWall.gd
@@ -4,5 +4,15 @@ onready var animation = $AnimationPlayer
 
 func _on_Area2D_body_entered(body):
 	print("WALL WAS HIT BY: ",body.name)
-	#animation.play("Break")
+	animation.play("Break")
 	pass # Replace with function body.
+
+const explosionEffect = preload("res://Effects/WallBreakEffect.tscn")
+
+func spawnExplosionEffect():
+	var explodeEffect = explosionEffect.instance()
+	explodeEffect.global_position = self.global_position
+	get_tree().get_root().add_child(explodeEffect)
+	print("boom")
+	# add_child_below_node(get_tree().get_root(),explodeEffect)
+	# explodeEffect.global_position = self.global_position
